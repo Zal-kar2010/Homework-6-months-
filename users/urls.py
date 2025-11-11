555657929ad9be0705edceb21423039f9ac13270
Zal-kar201 <zalkinator@gmail.com>
2025-11-11 15:59:41 +0600

    Сделал 4 дз

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from users.views import RegistrationAPIView, AuthorizationAPIView, ConfirmUserAPIView, CustomTokenObtainPairView
-
-urlpatterns = [
-    path('registration/', RegistrationAPIView.as_view(), name='registration'),
-    path('authorization/', AuthorizationAPIView.as_view(), name='authorization'),
-    path('confirm/', ConfirmUserAPIView.as_view(), name='confirm'),
-    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),  # ✅ путь для JWT
-]
